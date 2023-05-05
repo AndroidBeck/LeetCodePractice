f4b832f65918271d47c895bee483e5957b41fe3e
AEvd <alexandr8@gmail.com>
2023-05-05 18:19:42 +0300

    Solve task calculateMaxProfitV2 (from youtube algorithm interview)

--- src/main/kotlin/SomeOfMyOtherTasks.kt
+++ src/main/kotlin/SomeOfMyOtherTasks.kt
@@ -19,4 +19,41 @@ class SomeOfMyOtherTasks {
         }
         return String(result)
     }
+
+    // Every day we produce 1 point of goods. We can save it for later or trade it. We know the price for each day.
+    // Design algorithm calculating maximum gain
+    // Ex: 1 3 1 2 -> 10
+    fun calculateMaxProfit(prices: Array<Int>): Int {
+        var sum = 0
+        var localMax = prices.last()
+        var goodsForTradeQuantity = 0
+        for (i in prices.size -1 downTo 0) {
+            goodsForTradeQuantity++
+            if (prices[i] > localMax) {
+                // Trade for old price (local max)
+                sum += localMax * (goodsForTradeQuantity - 1)
+                //Save new local max
+                localMax = prices[i]
+                //Save current good for later trade
+                goodsForTradeQuantity = 1
+            }
+            // println("i = $i, current price = ${prices[i]}, local max = $localMax, quantity for trade = $goodsForTradeQuantity, sum = $sum")
+        }
+        // Final trade
+        sum += localMax * goodsForTradeQuantity
+        // println("After final trade: local max =  $localMax, quantity = $goodsForTradeQuantity, sum = $sum")
+        return sum
+    }
+
+    fun calculateMaxProfitV2(prices: Array<Int>): Int {
+        var sum = 0
+        var prevMax = 0
+        for (i in prices.size -1 downTo 0) {
+            if (prices[i] > prevMax) {
+                prevMax = prices[i]
+            }
+            sum += prevMax
+        }
+        return sum
+    }
 }
--- src/test/kotlin/SomeOfMyOtherTasksTest.kt
+++ src/test/kotlin/SomeOfMyOtherTasksTest.kt
@@ -11,4 +11,13 @@ class SomeOfMyOtherTasksTest {
         println(someOfMyOtherTasks.reverseDigitsInString(origStr).toCharArray())
         assertEquals("P5y4t3h2o1n", someOfMyOtherTasks.reverseDigitsInString(origStr))
     }
+
+    @Test
+    fun `test calculateMaxProfitV2`() {
+        val prices = arrayOf(1, 3, 1, 2)
+        assertEquals(10, someOfMyOtherTasks.calculateMaxProfitV2(prices))
+
+        val prices2 = arrayOf(10, 2, 6, 4, 3, 1, 2, 2, 2)
+        assertEquals(37, someOfMyOtherTasks.calculateMaxProfitV2(prices2))
+    }
 }
