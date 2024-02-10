a6fb1a7b97137f1bef917765b2ea67a98786b9ca
AEvd <alexandr8@gmail.com>
2024-02-10 17:31:26 +0300

    Solve task twoSum

--- src/main/kotlin/ArraySolution.kt
+++ src/main/kotlin/ArraySolution.kt
@@ -51,4 +51,34 @@ class ArraySolution {
 
     }
 
+    /*
+        https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546/
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+     */
+
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        for (i in 0 until nums.size - 1) {
+            for (j in i + 1 until nums.size) {
+                if (nums[i] + nums[j] == target) return intArrayOf(i, j)
+            }
+        }
+        return intArrayOf(-1)
+    }
+
+    fun twoSumV2(nums: IntArray, target: Int): IntArray {
+        val map = HashMap<Int, Int>()
+        nums.forEachIndexed { index, i ->
+            val goal = target - i
+            if (map[goal] != null) return intArrayOf(map[goal]!!, index)
+            map[i] = index
+        }
+        return intArrayOf(-1, -1)
+    }
+
 }
--- src/test/kotlin/ArraySolutionTest.kt
+++ src/test/kotlin/ArraySolutionTest.kt
@@ -1,3 +1,44 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 internal class ArraySolutionTest {
     private val arraySolution = ArraySolution()
+
+    @Test
+    fun `test toSum`() {
+        val nums = intArrayOf(2,7,11,15)
+        val target = 9
+        assertArrayEquals(intArrayOf(0, 1), arraySolution.twoSum(nums, target))
+
+        val nums2 = intArrayOf(3, 2, 4)
+        val target2 = 6
+        assertArrayEquals(intArrayOf(1, 2), arraySolution.twoSum(nums2, target2))
+
+        val nums3 = intArrayOf(3, 3)
+        val target3 = 6
+        assertArrayEquals(intArrayOf(0, 1), arraySolution.twoSum(nums3, target3))
+
+        val nums4 = intArrayOf(2,5,5,11)
+        val target4 = 10
+        assertArrayEquals(intArrayOf(1, 2), arraySolution.twoSum(nums4, target4))
+    }
+
+    @Test
+    fun `test toSumV2`() {
+        val nums = intArrayOf(2,7,11,15)
+        val target = 9
+        assertArrayEquals(intArrayOf(0, 1), arraySolution.twoSumV2(nums, target))
+
+        val nums2 = intArrayOf(3, 2, 4)
+        val target2 = 6
+        assertArrayEquals(intArrayOf(1, 2), arraySolution.twoSumV2(nums2, target2))
+
+        val nums3 = intArrayOf(3, 3)
+        val target3 = 6
+        assertArrayEquals(intArrayOf(0, 1), arraySolution.twoSumV2(nums3, target3))
+
+        val nums4 = intArrayOf(2,5,5,11)
+        val target4 = 10
+        assertArrayEquals(intArrayOf(1, 2), arraySolution.twoSumV2(nums4, target4))
+    }
 }
